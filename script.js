a3548c056923accc302988a1ab058935f5969416
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-08-13 20:45:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+describe('Background Image Test', function() {
+  it('Should have the correct background image', function() {
+    cy.visit('http://localhost:3000') // replace with your app's URL
+    cy.get('h1') // get the h1 element
+      .should('be.visible') // ensure it is visible (i.e., has loaded)
+      .should('have.css', 'background-image', 'url("https://www.globalfocusmagazine.com/wp-content/uploads/2020/02/Engaging_with_technology-scaled.jpg")'); // then check the background image
+  });
+});
